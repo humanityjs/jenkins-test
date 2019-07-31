a099e37f89703c0d7c9fc51b0cc1cde7810751a3
Daniel Bamidele <greatbolutife@gmail.com>
2019-08-01 00:07:37 +0100

    test test

--- before.sh
+++
@@ -1,4 +0,0 @@
-curl "https://api.github.com/repos/humanityjs/jenkins_test/statuses/4a7b428d58689468077ae9f30ab8342345086c51" \
-  -H "Content-Type: application/json" \
-  -X POST \
-  -d "{\"state\": \"pending\", \"description\": \"Jenkins\", \"target_url\": \"http://brevis.serveo.net/job/jenkins-test/7/console\"}"
